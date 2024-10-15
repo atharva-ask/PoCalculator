269addf0e57852b5e8c412d8c3e4f694998d980c
atharva-ask <112977527+atharva-ask@users.noreply.github.com>
2024-10-15 23:45:09 +0530

    vercelfinal5

--- backend/database.py
+++ backend/database.py
@@ -1,14 +1,5 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 import psycopg2
 DB_URL = 'postgresql://default:2ytpuU7swTql@ep-young-salad-a10bjpmp-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require'
-
-db_name = "internship_db_2gps"
-db_user = "internship_db_2gps_user"
-db_pass = "oP5esjf28Xc7AWCuCFVBSaqT9LkSClMd"
-db_host = "dpg-cs5plslumphs73b0qhs0-a"
-db_port = "5432"
 
 POSTGRES_URL="postgres://default:2ytpuU7swTql@ep-young-salad-a10bjpmp-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require"
 POSTGRES_PRISMA_URL="postgres://default:2ytpuU7swTql@ep-young-salad-a10bjpmp-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require&pgbouncer=true&connect_timeout=15"
@@ -31,11 +22,3 @@ def get_database_conn():
     except Exception as e:
         print("Database Connection Error:", e)
         return None
-
-
-
-
-engine = create_engine(DB_URL)
-sessionLocal = sessionmaker(bind = engine)
-
-Base = declarative_base()
--- backend/main.py
+++ backend/main.py
@@ -272,6 +272,9 @@ def getacheivedPoAttainment(year : int):
 
 
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
 
